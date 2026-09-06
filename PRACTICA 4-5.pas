44c4629033d2d464fdc7ce13b10a929ca33e4ce2
Marco Mendoza Matus <marcomendozamatus07@gmail.com>
2026-09-06 20:45:49 -0300

    Add files via upload

---
+++ PRACTICA 4-5.pas
@@ -0,0 +1,39 @@
+{5. Una veterinaria desea procesar la información de las consultas realizadas durante el año.
+De cada consulta se conoce: número de consulta, número de historia clínica de la mascota,
+fecha, tipo de consulta y costo de la consulta. La lectura de las consultas finaliza cuando se
+ingresa el número de consulta -1.
+
+Implementar un programa que invoque a los siguientes módulos y compruebe el correcto
+funcionamiento del mismo.
+a. Un módulo que retorne la información de las historias clínicas en un árbol binario de
+búsqueda ordenado por número de historia clínica. Para cada historia clínica se debe
+almacenar una lista con las consultas realizadas a la mascota correspondiente.
+b. Un módulo que imprima recursivamente todas las historias clínicas en orden creciente
+de número.
+c. Un módulo que reciba el árbol y retorne el número de historia clínica con mayor
+cantidad de consultas.
+d. Un módulo que reciba el árbol y un número de historia clínica. Debe retornar la
+cantidad total de consultas realizadas a dicha mascota. En caso
+e. Un módulo que reciba el árbol y un valor de costo. Debe retornar la cantidad de
+consultas cuyo costo supera el valor recibido.
+f. Un módulo que reciba el árbol y dos números de historia clínica. Debe retornar el costo
+total de las consultas correspondientes a las historias clínicas comprendidas entre
+ambos números, inclusive.
+g. Un módulo que reciba el árbol y genere una nueva estructura ordenada por número de
+historia clínica, donde cada historia aparezca una única vez junto con el costo total
+acumulado de sus consultas.
+}
+
+
+program practica4_5;
+
+type
+
+
+var
+
+BEGIN
+	
+	
+END.
+
