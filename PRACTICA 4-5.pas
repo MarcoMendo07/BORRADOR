735161183ced6d8249ca2d77414739448810f0f3
Marco Mendoza Matus <marcomendozamatus07@gmail.com>
2026-09-07 09:34:16 -0300

    Add files via upload

--- PRACTICA 4-5.pas
+++ PRACTICA 4-5.pas
@@ -1,13 +1,17 @@
 {5. Una veterinaria desea procesar la información de las consultas realizadas durante el año.
+
+
 De cada consulta se conoce: número de consulta, número de historia clínica de la mascota,
 fecha, tipo de consulta y costo de la consulta. La lectura de las consultas finaliza cuando se
 ingresa el número de consulta -1.
 
 Implementar un programa que invoque a los siguientes módulos y compruebe el correcto
 funcionamiento del mismo.
+
 a. Un módulo que retorne la información de las historias clínicas en un árbol binario de
 búsqueda ordenado por número de historia clínica. Para cada historia clínica se debe
 almacenar una lista con las consultas realizadas a la mascota correspondiente.
+
 b. Un módulo que imprima recursivamente todas las historias clínicas en orden creciente
 de número.
 c. Un módulo que reciba el árbol y retorne el número de historia clínica con mayor
@@ -29,10 +33,93 @@ program practica4_5;
 
 type
 
+{De cada consulta se conoce: número de consulta, número de historia clínica de la mascota,
+fecha, tipo de consulta y costo de la consulta. La lectura de las consultas finaliza cuando se
+ingresa el número de consulta -1.}}
+
+infoFecha = record
+dia: integer;
+mes: integer;
+anio: integer;
+end;
+
+consulta = record
+numero: integer;
+numHistoria: integer;
+fecha: infoFecha;
+tipo: integer;
+costo: real;
+end;
+
+// ARBOL 1
+
+infoLista = record;
+numero: integer;
+fecha: infoFecha;
+tipo: integer;
+costo: real;
+end;
+
+listaConsultas = ^nodo
+nodo = record
+dato: infoLista;
+sig: listaConsultas;
+end;
+
+infoHistoria = record
+numHistoria: integer;
+lista: listaConsultas;
+end;
+
+arbol = ^nodoArbol;
+nodoArbol = record
+dato: infoHistoria;
+HI: arbol;
+HD: arbol;
+end;
+
+{a. Un módulo que retorne la información de las historias clínicas en un árbol binario de
+búsqueda ordenado por número de historia clínica. Para cada historia clínica se debe
+almacenar una lista con las consultas realizadas a la mascota correspondiente.}
+
+procedure leerConsulta(var c: consulta);
+begin
+	writeln('Ingrese un numero de consulta');
+	readln(c.numero);
+	if(c.numero <> -1)then begin
+			writeln('Ingrese un numero de historia clinica');
+	        readln(c.numHistoria);
+			writeln('Ingrese un dia de consulta');
+	        readln(c.fecha.dia);
+			writeln('Ingrese un mes de consulta');
+	        readln(c.fecha.mes);
+			writeln('Ingrese un año de consulta');
+	        readln(c.fecha.anio);	
+			writeln('Ingrese un tipo de consulta');
+	        readln(c.tipo);
+			writeln('Ingrese un costo de consulta');
+	        readln(c.costo);
+	end;
+end;
+
+
+procedure generarArbol(var a: arbol);
+var
+c: consulta;
+begin
+	leerConsulta(c);
+	while(c.numero <> -1)do begin
+		leerConsulta(c);
+		insertarNodo(a,c);
+	end;
+end;
 
 var
+a: arbol;
 
 BEGIN
+a:= nil;
+generarArbol(a);
 	
 	
 END.
